2093131f18dbe1efad6e5a650c1bdd750fcb398b
Dhanesh <66869375+dhaneshsaini@users.noreply.github.com>
2022-01-04 09:18:07 +0530

    v1.1
    mobile responsive

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ let inp = document.querySelectorAll('input');
 let card = document.querySelector('.card');
 let cs = card.style;
 
+cssOut.innerText = `
+/* https://frontendweb.site/ */
+
+background: rgba(255, 255, 255, 0.2);
+border-radius: 16px;
+box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+backdrop-filter: blur(5px);
+-webkit-backdrop-filter: blur(5px);
+border: 1px solid rgba(255, 255, 255, 0.3);
+`;
+
 const hexToRgb = hex =>
     hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
         , (m, r, g, b) => '#' + r + r + g + g + b + b)
@@ -14,7 +25,6 @@ function change() {
     cs.backdropFilter = `blur(${inp[1].value}px)`;
     cs.border = `1px solid rgba(${hexToRgb(inp[0].value)},${inp[3].value})`;
 
-
     cssOut.innerText = `
 /* https://frontendweb.site/ */
 
@@ -25,4 +35,12 @@ backdrop-filter: blur(${inp[1].value}px);
 -webkit-backdrop-filter: blur(${inp[1].value}px);
 border: 1px solid rgba(${hexToRgb(inp[0].value)},${inp[3].value});
 `;
+}
+
+// copy code
+const copyBtn = document.querySelector('.copyBtn');
+copyBtn.onclick = () => {
+    cssOut.select();
+    cssOut.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(cssOut.innerText);
 }
